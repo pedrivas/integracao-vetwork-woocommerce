c292b4bebf34899c88870332709e1593e0900f11
pedrivas <42252328+pedrivas@users.noreply.github.com>
2020-10-15 22:22:28 -0300

    criacao do depara dos ids

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,26 @@
 const Excel = require('exceljs');
 
-const workbook = new Excel.Workbook();
+const deparaWorkbook = new Excel.Workbook();
+const productsWorkbook = new Excel.Workbook();
 
-const filename = './assets/2020-10-14_lista_produtos.xlsx'
-workbook.xlsx.readFile(filename).then(function() {
-  var worksheet = workbook.getWorksheet("Produtos");
+var dePara = {};
 
+const productsFileName = './assets/2020-10-14_lista_produtos.xlsx'
+const deParaFileName = './assets/DEPARA_PRODUTOS.xlsx'
+
+deparaWorkbook.xlsx.readFile(deParaFileName).then(() => {
+  var worksheet = deparaWorkbook.getWorksheet("DEPARA");
+  var row = null;
+  var idVetworkList = null;
+  var stockQuantityList = null;
+  idVetworkList = worksheet.getColumn(1).values 
+  stockQuantityList = worksheet.getColumn(7).values 
+
+  }
+)
+
+productsWorkbook.xlsx.readFile(productsFileName).then(() => {
+  var worksheet = productsWorkbook.getWorksheet("Produtos");
   var row = null;
   var idVetworkList = null;
   var stockQuantityList = null;
