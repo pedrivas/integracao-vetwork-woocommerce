d48628af96717dac137418b3ff38242696501f63
pedrivas <42252328+pedrivas@users.noreply.github.com>
2020-11-25 22:18:19 -0300

    Loop de requisicoes API WooCommerce

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,57 +7,60 @@ const api = new WooCommerceRestApi({
   consumerSecret: "cs_4ec3d70b4a2631884bb4b7b4740822135dac4121",
   version: "wc/v3"
 });
-// const Excel = require('exceljs');
+const Excel = require('exceljs');
 
-// const deparaWorkbook = new Excel.Workbook();
-// const productsWorkbook = new Excel.Workbook();
+const deparaWorkbook = new Excel.Workbook();
+const productsWorkbook = new Excel.Workbook();
 
-// var dePara = {};
+var dePara = [];
 
 // const productsFileName = './assets/2020-10-14_lista_produtos.xlsx'
-// const deParaFileName = './assets/DEPARA_PRODUTOS.xlsx'
+const deParaFileName = './assets/DEPARA_PRODUTOS.xlsx'
 
-// deparaWorkbook.xlsx.readFile(deParaFileName).then(() => {
-//   var worksheet = deparaWorkbook.getWorksheet("DEPARA");
-//   var row = null;
-//   var idVetworkList = null;
-//   var stockQuantityList = null;
-//   idVetworkList = worksheet.getColumn(1).values 
-//   stockQuantityList = worksheet.getColumn(7).values 
+deparaWorkbook.xlsx.readFile(deParaFileName).then(() => {
+  var worksheet = deparaWorkbook.getWorksheet("DEPARA");
+  var row = null;
+  var idWooList = null
+  var quantityList = null
 
-//   }
-// )
-// //worksheet.getRow(8)._cells
+  var stockQuantityList = null;
+  idWooList = worksheet.getColumn(2).values 
+  quantityList = worksheet.getColumn(4).values 
 
-// productsWorkbook.xlsx.readFile(productsFileName).then(() => {
-//   var worksheet = productsWorkbook.getWorksheet("Produtos");
-//   var row = null;
-//   var idVetworkList = null;
-//   var stockQuantityList = null;
-//   idVetworkList = worksheet.getColumn(1).values 
-//   stockQuantityList = worksheet.getColumn(7).values 
+  for (var numberReg = 2; numberReg <= idWooList.length; numberReg++ ){
+    dePara.push([idWooList[numberReg],quantityList[numberReg]])
+  }
+  updateQuantity();
+  }
+)
 
-//   }
-// )
+async function updateQuantity(){
+  var requestBody = null
+  for(var product = 0;product <= dePara.length;product++ ) {
+    requestBody = JSON.stringify({
+      manage_stock: true,
+      stock_quantity: dePara[product][1]
+    })
+    requestBody = JSON.parse(requestBody)
+    await api.put(`products/${dePara[product][0]}`,requestBody)
+    .then((response) => {
+          // Successful request
+          console.log("Response Status:", response.status);
+          console.log("Response Headers:", response.headers);
+          console.log("Response Data:", response.data);
+          console.log("Total of pages:", response.headers['x-wp-totalpages']);
+          console.log("Total of items:", response.headers['x-wp-total']);
+        })
+        .catch((error) => {
+          // Invalid request, for 4xx and 5xx statuses
+          console.log("Response Status:", error.response.status);
+          console.log("Response Headers:", error.response.headers);
+          console.log("Response Data:", error.response.data);
+        })
+        .finally(() => {
+          // Always executed.
+        });
 
-// List products
-api.get("products", {
-  per_page: 20, // 20 products per page
-})
-  .then((response) => {
-    // Successful request
-    console.log("Response Status:", response.status);
-    console.log("Response Headers:", response.headers);
-    console.log("Response Data:", response.data);
-    console.log("Total of pages:", response.headers['x-wp-totalpages']);
-    console.log("Total of items:", response.headers['x-wp-total']);
-  })
-  .catch((error) => {
-    // Invalid request, for 4xx and 5xx statuses
-    console.log("Response Status:", error.response.status);
-    console.log("Response Headers:", error.response.headers);
-    console.log("Response Data:", error.response.data);
-  })
-  .finally(() => {
-    // Always executed.
-  });
+  }
+}
+
